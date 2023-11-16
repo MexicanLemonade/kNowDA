b4ffa3c37436ecd9e13b06855ae5916cf24eaf9f
MexicanLemonade <yikangpan2001@gmail.com>
2023-11-15 21:26:32 -0500

    change frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def main():
             with open(uploaded_file.name, "rb") as f:
                 import base64
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
                 st.write(pdf_display, unsafe_allow_html=True)
 
             # To read file as bytes and then display it as a download link:
