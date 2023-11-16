903d51ba848f1f7e99b10496814a5526180d7c74
MexicanLemonade <yikangpan2001@gmail.com>
2023-11-15 22:34:51 -0500

    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,5 @@
 import streamlit as st
 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
-    pdf_display =  f"""<embed
-    class="pdfobject"
-    type="application/pdf"
-    title="Embedded PDF"
-    src="data:application/pdf;base64,{base64_pdf}"
-    style="overflow: auto; width: 100%; height: 100%;">"""
-
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
 def main():
     st.title('kNowDA: know your NDAs before you sign them')
 
@@ -31,6 +15,14 @@ def main():
             st.write("filename:", uploaded_file.name)
             st.write("filetype:", uploaded_file.type)
             st.write("filesize:", uploaded_file.size)
+
+            # To render the uploaded file 
+            st.write("Preview of PDF file:")
+            with open(uploaded_file.name, "rb") as f:
+                import base64
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                st.write(pdf_display, unsafe_allow_html=True)
 
             # To read file as bytes and then display it as a download link:
             with open(uploaded_file.name, "wb") as f:
