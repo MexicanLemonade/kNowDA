e458920086e0785d24a109467c9d9a125563ecc6
MexicanLemonade <yikangpan2001@gmail.com>
2023-11-15 21:12:18 -0500

    change to pdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,34 @@
 import streamlit as st
-from io import StringIO
-import pandas as pd
 
 def main():
-    st.title('File Upload Web Application')
+    st.title('PDF File Upload Web Application')
 
     # Instructions for user
-    st.write('Upload a CSV file for analysis.')
+    st.write('Upload a PDF file.')
 
-    # File uploader allows user to add their own CSV
-    uploaded_file = st.file_uploader('Upload your input CSV file', type=['csv'])
+    # File uploader allows user to add their own PDF
+    uploaded_file = st.file_uploader('Upload your input PDF file', type=['pdf'])
 
     if uploaded_file is not None:
         # Check the file's extension
-        if uploaded_file.name.endswith('.csv'):
-            # To read file as bytes:
-            bytes_data = uploaded_file.getvalue()
+        if uploaded_file.name.endswith('.pdf'):
             st.write("filename:", uploaded_file.name)
             st.write("filetype:", uploaded_file.type)
             st.write("filesize:", uploaded_file.size)
 
-            # To convert to a string based IO:
-            stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
-            st.write(stringio)
-
-            # To read file as string:
-            string_data = stringio.read()
-            st.write(string_data)
-
-            # Can be used wherever a "file-like" object is accepted:
-            dataframe = pd.read_csv(uploaded_file)
-            st.write(dataframe)
+            # To read file as bytes and then display it as a download link:
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            
+            st.write('PDF file is uploaded successfully.')
+            st.download_button(
+                label="Download PDF",
+                data=uploaded_file.getvalue(),
+                file_name=uploaded_file.name,
+                mime='application/octet-stream'
+            )
         else:
-            st.error('Invalid file type: Please upload a CSV file.')
+            st.error('Invalid file type: Please upload a PDF file.')
 
 if __name__ == '__main__':
     main()
