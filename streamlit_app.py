6898d4c4bf2434b07c30ea1595078e58e9fc2bd9
MexicanLemonade <yikangpan2001@gmail.com>
2023-11-15 22:18:47 -0500

    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,11 @@ def main():
 
             # To render the uploaded file 
             st.write("Preview of PDF file:")
-            displayPDF(uploaded_file.name)
+            with open(uploaded_file.name, "rb") as f:
+                import base64
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                st.write(pdf_display, unsafe_allow_html=True)
 
             # To read file as bytes and then display it as a download link:
             with open(uploaded_file.name, "wb") as f:
