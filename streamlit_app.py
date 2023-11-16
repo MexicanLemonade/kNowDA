a3ab9c056e858bcdecc0c00a5c3e83b42b390b38
MexicanLemonade <yikangpan2001@gmail.com>
2023-11-15 21:21:07 -0500

    pdf render

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title('PDF File Upload Web Application')
+    st.title('kNowDA: know your NDAs before you sign them')
 
     # Instructions for user
     st.write('Upload a PDF file.')
@@ -15,6 +15,13 @@ def main():
             st.write("filename:", uploaded_file.name)
             st.write("filetype:", uploaded_file.type)
             st.write("filesize:", uploaded_file.size)
+
+            # To render the uploaded file 
+            st.write("Preview of PDF file:")
+            with open(uploaded_file.name, "rb") as f:
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                st.write(pdf_display, unsafe_allow_html=True)
 
             # To read file as bytes and then display it as a download link:
             with open(uploaded_file.name, "wb") as f:
